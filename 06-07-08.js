61a577d7a12d7c171f9c6fe9e091b9a2464d4e53
Fernando Ramones <fernandoramones92@gmail.com>
2022-10-06 00:45:46 -0300

    will vi-vii-viiiF

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,7 +11,10 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
-
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
     }
 
     addFriend(nombre, edad) {
@@ -20,15 +23,16 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
-
+      var amigo = { nombre, edad };
+      this.amigos.push(amigo);
     }
 
     addHobby(hobby) {
       // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
       // No debe retornar nada.
-
       // Tu código aca:
-
+      
+      this.hobbies.push(hobby);
     }
     getFriends() {
       // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
@@ -36,18 +40,20 @@ function crearClasePersona() {
       // Ej:
       // Suponiendo que la persona tiene estos amigos: [{nombre: 'martin', edad: 31},{nombre: 'toni', edad: 33}]
       // persona.getFriends() debería devolver ['martin', 'toni']
-
       // Tu código aca:
 
+      const transformarObjetoAmigoANombre = ((amigo) => amigo.nombre);
+      const indexed = this.amigos.map(transformarObjetoAmigoANombre);
+      return indexed
     }
 
     getHobbies() {
       // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
-
       // Tu código aca:
-
+      
+      return this.hobbies;
     }
 
     getPromedioEdad() {
@@ -64,15 +70,15 @@ function crearClasePersona() {
       //   }]
       // }
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
-
       // Tu código aca:
-
+      return this.amigos.map(amigo => amigo.edad).reduce((a, b) => a + b) / this.amigos.length
+    
     }
-  };
+  }
 
   return Persona;
 }
 
 // No modifiques nada debajo de esta linea //
 
-module.exports = crearClasePersona
+module.exports = crearClasePersona;
